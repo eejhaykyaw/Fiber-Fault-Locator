ddf143fd5d7a9cb6c56e8dbfb9b54020686ef65c
eejhaykyaw <quiao.erfejr@gmail.com>
2026-04-01 18:46:29 +0800

    Add files via upload
    pwa enabling files

---
+++ service-worker.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'fiber-fault-locator-v1';
+const URLS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  'https://unpkg.com/leaflet/dist/leaflet.css',
+  'https://unpkg.com/leaflet/dist/leaflet.js',
+  'https://unpkg.com/@tmcw/togeojson',
+  'https://cdnjs.cloudflare.com/ajax/libs/jszip/3.7.1/jszip.min.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
+});
